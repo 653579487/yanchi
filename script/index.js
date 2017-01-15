abbfa935be67d68d56c7ec3f985662266212af95
653579487 <605895665@qq.com>
2017-01-15 20:29:58 +0800

    '测试'

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by Administrator on 2017/1/15 0015.
  */
+!function(){
+   var td = document.querySelectorAll('td');
+    for(var i =0; i<td.length; i++){
+      td[i].className =td[i].className + ' ' +'td';
+    }
+}();
